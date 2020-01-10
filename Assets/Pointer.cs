3e3e82b382afb09250b3d2da5635fe6da03d3972
Tim Peeters <tim.peeters12345@gmail.com>
2020-01-10 12:56:06 +0100

    Unity Project

---
+++ Assets/Pointer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Pointer : MonoBehaviour
+{
+	[SerializeField] private float m_defaultLenght = 5.0f;
+	[SerializeField] private GameObject m_dot;
+	[SerializeField] private InputModule m_inputModule;
+
+	private LineRenderer m_lineRenderer = null;
+
+	private void Awake()
+    {
+		m_lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+		UpdateLine();
+    }
+
+	private void UpdateLine()
+	{
+		float _targetLength = m_defaultLenght;
+		RaycastHit _hit = CreateRaycast(_targetLength);
+		Vector3 _endPosition = transform.position + (transform.position * _targetLength);
+		if(_hit.collider != null)
+		{
+			_endPosition = _hit.point;
+		}
+
+		m_dot.transform.position = _endPosition;
+
+		m_lineRenderer.SetPosition(0, transform.position);
+		m_lineRenderer.SetPosition(1, _endPosition);
+	}
+
+	private RaycastHit CreateRaycast(float _length)
+	{
+		RaycastHit _hit;
+		Ray _ray = new Ray(transform.position, transform.forward);
+		Physics.Raycast(_ray, out _hit, m_defaultLenght);
+
+		return _hit;
+	}
+}
---
+++ Assets/Script/RGBGame.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RGBGame : MonoBehaviour
+{
+    [SerializeField] bool isImage;
+
+    [SerializeField] private Text textComponent;
+
+    [SerializeField] private Image imageComponent;
+
+    [SerializeField] private Vector2 red;
+
+    [SerializeField] private Vector2 blue;
+    
+    [SerializeField] private Vector2 green;
+
+    [SerializeField] private Vector2 alpha;
+
+    [SerializeField] private Transform origin;
+    private Renderer ren;
+
+    private void Start()
+    {
+        ren = GetComponent<Renderer>();
+    }
+
+    private void Update()
+    {
+
+        red.x = Mathf.Abs((origin.transform.position - transform.position).x) / red.y;
+
+        green.x = Mathf.Abs((origin.transform.position - transform.position).y) / green.y;
+
+        blue.x = Mathf.Abs((origin.transform.position - transform.position).z) / blue.y;
+
+        alpha.x = Vector3.Distance(origin.transform.position, transform.position) / alpha.y;
+
+        if (isImage)
+        {
+            imageComponent.color = new Color(Mathf.Clamp(red.x, 0, 1), Mathf.Clamp(green.x, 0, 1), Mathf.Clamp(blue.x, 0, 1));
+        }
+        else
+        {
+            
+
+            textComponent.color  = new Color(Mathf.Clamp(red.x, 0, 1), Mathf.Clamp(green.x, 0, 1), Mathf.Clamp(blue.x, 0, 1), Mathf.Clamp(alpha.x, 0, 1));
+        }
+
+
+    }
+}
