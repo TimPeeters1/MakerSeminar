a64c1e4a2ac1cb868b39642f6595500860e8333c
pimhoreman@gmail.com <pimhoreman@gmail.com>
2020-01-15 12:04:46 +0100

    I Pusehd EscapeTimer.cs

---
+++ Assets/Scripts/EscapeTimer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EscapeTimer : MonoBehaviour
+{
+	[SerializeField] private Text timer;
+
+	[SerializeField] private float timeLeft = 600;
+
+	private int min;
+
+	private int sec;
+
+	private void Start()
+	{
+		min = Mathf.FloorToInt(timeLeft / 60);
+
+		sec = Mathf.FloorToInt(timeLeft % 60);
+
+		timer.text = min.ToString("00" + ":" + sec.ToString("00"));
+	}
+
+
+	private void Update()
+	{
+		TimerUpdate();
+	}
+
+	private void TimerUpdate()
+	{
+		min = Mathf.FloorToInt(timeLeft / 60);
+
+		sec = Mathf.FloorToInt(timeLeft % 60);
+
+		timer.text = min.ToString("00") + ":" + sec.ToString("00");
+
+		timeLeft -= Time.deltaTime;
+
+		if (timeLeft < 0)
+		{
+			//DO IETS, LOCK DE DEUR OFZO
+		}
+	}
+}
