5d6d0227f348a3bc305921b5c8e5e344edf0ef94
pimhoreman@gmail.com <pimhoreman@gmail.com>
2020-01-15 14:25:34 +0100

    i htink this works

--- Assets/Scripts/ControllerTrackPad.cs
+++ Assets/Scripts/ControllerTrackPad.cs
@@ -9,33 +9,42 @@ public class ControllerTrackPad : MonoBehaviour
 	public SteamVR_ActionSet set;
 	public SteamVR_Input_Sources inputSource;
 	public float scrollSpeed = 5f;
-
+	public float maxY, minY, maxX, minX;
 	public Image scrollImage;
+
+	private float scrollImageX;
+	private float scrollImageY;
 
 	private void Start()
     {
 		scrollImage = GetComponent<Image>();
-    }
+		scrollImageX = scrollImage.transform.position.x;
+		scrollImageY = scrollImage.transform.position.y;
+	}
 
 	private void Update()
     {
 		if (SteamVR_Actions.default_TPDown.state)
 		{
+			scrollImageY = Mathf.Clamp(scrollImageY, minY, maxY);
 			scrollImage.transform.position += Vector3.up * scrollSpeed * Time.deltaTime;
 		}
 
 		if (SteamVR_Actions.default_TPUp.state)
 		{
+			scrollImageY = Mathf.Clamp(scrollImageY, minY, maxY);
 			scrollImage.transform.position += Vector3.down * scrollSpeed * Time.deltaTime;
 		}
 
 		if (SteamVR_Actions.default_TPLeft.state)
-		{
+		{ 
+			scrollImageX = Mathf.Clamp(scrollImageX, minX, maxX);
 			scrollImage.transform.position += Vector3.right * scrollSpeed * Time.deltaTime;
 		}
 
 		if (SteamVR_Actions.default_TPRight.state)
 		{
+			scrollImageX = Mathf.Clamp(scrollImageX, minX, maxX);
 			scrollImage.transform.position += Vector3.left * scrollSpeed * Time.deltaTime;
 		}
 	}
